1d639ca7cf3d46fc01dc49a441c2d68080e80dd6
Blaz Grilc <blaz@spletnik.si>
2011-01-07 16:08:39 +0100

    fixed activesupport require to work with rails 3.x and bumped version to 1.0.8

--- lib/epp.rb
+++ lib/epp.rb
@@ -2,7 +2,7 @@
 require 'rubygems'
 require 'openssl'
 require 'socket'
-require 'activesupport'
+require 'active_support'
 require 'rexml/document'
 require 'hpricot'
 
@@ -12,5 +12,5 @@ require File.dirname(__FILE__) + '/epp/server.rb'
 require File.dirname(__FILE__) + '/epp/exceptions.rb'
 
 module Epp #:nodoc:
-  VERSION = '1.0.6'
+  VERSION = '1.0.8'
 end
